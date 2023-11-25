94989e9c9cd154e7edb9c1169d57a7c652d9f026
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-11-25 21:41:43 +0100

    algorithm solved

--- atfirst.js
+++ atfirst.js
@@ -1,8 +1,18 @@
-String-1 -- atFirst
-Given a string, return a string length 2 made of its first 2 chars. If the string length is less than 2, use '@' for the missing chars.
+// String-1 -- atFirst
+// Given a string, return a string length 2 made of its first 2 chars. If the string length is less than 2, use '@' for the missing chars.
 
-Examples
+// Examples
 
-atFirst('hello') → he
-atFirst('hi') → hi
-atFirst('h') → h@
+// atFirst('hello') → he
+// atFirst('hi') → hi
+// atFirst('h') → h@
+
+function atFirst(str){
+    if(str.length >0 && str.length < 2){
+      return str+"@"
+    }if(str.length === 0){
+      return "@"+"@"
+    }if(str.length >= 2){
+      return str.slice(0,2)
+    }
+  }console.log(atFirst("hello"));
